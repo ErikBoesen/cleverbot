3ddcce8e5601855713f454e1663277bc814db683
ErikBoesen <me@erikboesen.com>
2019-10-17 11:08:10 -0400

    Don't respond verbatim

--- bot.py
+++ bot.py
@@ -9,34 +9,34 @@ app = Flask(__name__)
 bot = mebots.Bot('cleverbot', os.environ.get('BOT_TOKEN'))
 
 class CleverBot:
-    _API_ROOT = "https://cleverbot.io/1.0/"
+    _API_ROOT = 'https://cleverbot.io/1.0/'
 
     def __init__(self, user, key, group_id):
         self.user = user
         self.key = key
-        self.nick = f"GroupMe_{group_id}"
+        self.nick = f'GroupMe_{group_id}'
 
         body = {
-            "user": user,
-            "key": key,
-            "nick": self.nick
+            'user': user,
+            'key': key,
+            'nick': self.nick
         }
-        requests.post(self._API_ROOT + "create", data=body)
+        requests.post(self._API_ROOT + 'create', data=body)
 
     def query(self, text):
         body = {
-            "user": self.user,
-            "key": self.key,
-            "nick": self.nick,
-            "text": text
+            'user': self.user,
+            'key': self.key,
+            'nick': self.nick,
+            'text': text
         }
 
-        r = requests.post(self._API_ROOT + "ask", data=body).json()
+        r = requests.post(self._API_ROOT + 'ask', data=body).json()
         if not r.ok:
-            return "Cleverbot API appears to be offline. If cleverbot.io is online, something may have gone wrong. Open an issue at https://github.com/ErikBoesen/cleverbot/issues/new if so."
+            return 'Cleverbot API appears to be offline. If cleverbot.io is online, something may have gone wrong. Open an issue at https://github.com/ErikBoesen/cleverbot/issues/new if so.'
 
-        if r["status"] == "success":
-            return r["response"]
+        if r['status'] == 'success':
+            return r['response']
         else:
             return None
 
@@ -49,20 +49,20 @@ clients = {}
 
 def get_response(text, group_id):
     if clients.get(group_id) is None:
-        clients[group_id] = CleverBot(os.environ.get("CLEVERBOT_USER"), os.environ.get("CLEVERBOT_KEY"), group_id)
+        clients[group_id] = CleverBot(os.environ.get('CLEVERBOT_USER'), os.environ.get('CLEVERBOT_KEY'), group_id)
     return clients[group_id].query(text)
 
 @app.route('/', methods=['POST'])
 def receive():
     data = request.get_json()
-    group_id = data.get("group_id")
+    group_id = data.get('group_id')
     print('Incoming message:')
     print(data)
 
     # Prevent self-reply
     if data['sender_type'] != 'bot':
         if data['text'].startswith(PREFIX):
-            send(data['text'][len(PREFIX):].strip(), group_id)
+            send(get_response(data['text'][len(PREFIX):].strip(), group_id), group_id)
 
     return 'ok', 200
 
@@ -77,12 +77,12 @@ def send(msg, group_id):
     r = requests.post(url, data=data)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("command", nargs="?")
+    parser.add_argument('command', nargs='?')
     args = parser.parse_args()
     if args.command:
         print(get_response({'text': args.command, 'sender_type': 'bot'}, 0))
     else:
         while True:
-            print(get_response({'text': input("> "), 'sender_type': 'bot'}, 0))
+            print(get_response({'text': input('> '), 'sender_type': 'bot'}, 0))
